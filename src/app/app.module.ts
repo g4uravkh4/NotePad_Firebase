e8ee93d36a398a04712614792f4b8f25ccd98141
aura-gv <gauravkhatiwada4@gmail.com>
2023-07-01 08:00:47 +0545

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteComponent } from './note/note.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NoteComponent],
+  imports: [BrowserModule, AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideFirestore(() => getFirestore())],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'crud-note-cd5ce',
+    appId: '1:1083462429434:web:5935125e1e20588da27743',
+    storageBucket: 'crud-note-cd5ce.appspot.com',
+    apiKey: 'AIzaSyCStloo_mxcOQcT6w3G3usFhcXyDWMdvaI',
+    authDomain: 'crud-note-cd5ce.firebaseapp.com',
+    messagingSenderId: '1083462429434',
+  },
+};
